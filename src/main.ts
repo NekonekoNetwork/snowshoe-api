82174f23764f175e2b1e2b6026e432e4d917c802
Nekoneko <nekonekogame@gmail.com>
2022-06-29 12:32:56 +0000

    Remove PrismaClientExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { AppModule } from '@app/app.module';
-import { PrismaClientExceptionFilter } from '@app/prisma/prisma-client-exception.filter';
 import { PrismaService } from '@app/prisma/prisma.service';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
-
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(4000);
 }
