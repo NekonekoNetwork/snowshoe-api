9dd1998ab1cabbe9d9931f1cdf8078787774f050
Nekoneko <nekonekogame@gmail.com>
2022-10-19 12:48:53 +0000

    VirtualHostModuleの有効化

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { FallbackModule } from '@app/common/fallback/fallback.module';
 import { NamespaceModule } from '@app/common/namespace/namespace.module';
 import { ServerStatusModule } from '@app/common/server-status/server-status.module';
 import { ServerModule } from '@app/common/server/server.module';
-// import { VirtualHostModule } from '@app/common/virtual-host/virtual-host.module';
+import { VirtualHostModule } from '@app/common/virtual-host/virtual-host.module';
 import { PrismaModule } from '@app/prisma/prisma.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
@@ -21,7 +21,7 @@ import { join } from 'path';
     NamespaceModule,
     ServerModule,
     ServerStatusModule,
-    // VirtualHostModule,
+    VirtualHostModule,
     FallbackModule,
   ],
 })
